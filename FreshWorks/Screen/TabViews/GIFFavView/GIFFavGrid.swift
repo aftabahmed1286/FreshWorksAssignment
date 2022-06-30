7e0286c9c2a605ad121690925c54fbc08d37ce48
Aftab Ahmed <aahmed03@in.ibm.com>
2022-06-30 20:02:07 +0530

    Bug fix.

--- FreshWorks/Screen/TabViews/GIFFavView/GIFFavGrid.swift
+++ FreshWorks/Screen/TabViews/GIFFavView/GIFFavGrid.swift
@@ -12,23 +12,17 @@ struct GIFFavGrid: View {
     var gifImageData: [GIFImageData]
     var rows: Int
     
-    @State var shouldRefresh = false
-    
     var body: some View {
-        if shouldRefresh == true || shouldRefresh == false {
         ScrollView {
             VStack(spacing: 20) {
                 ForEach(0..<rows, id: \.self) { row in
                     if row % 2 == 0 {
-                        GridEvenRow(row: row, shouldRefresh: $shouldRefresh)
+                        GridEvenRow(row: row)
                     } else {
                         GridOddRow(row: row)
                     }
                 }
             }
-        }
-        .padding([.leading, .trailing], 20)
-        }
-        
+        }.padding([.leading, .trailing], 20)
     }
 }
